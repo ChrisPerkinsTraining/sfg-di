903927145ed09476182e4bd7b9afbb675eabe365
ChrisPerkinsCap <chrisperkins.cap@gmail.com>
2023-04-13 12:51:08 +0200

    'setterInjectedController' implemented with annotations @controller on the class and @Autowired on the setter method.

--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -1,11 +1,15 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
 
     public GreetingsService greetingsService;
 
+    @Autowired
     public void setGreetingService( GreetingsService greetingsService ) {
 
         this.greetingsService = greetingsService;
