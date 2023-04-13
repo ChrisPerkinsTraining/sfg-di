012f50162ba198a57434426d510abcb94ac483af
ChrisPerkinsCap <chrisperkins.cap@gmail.com>
2023-04-13 22:13:54 +0200

    Implemented @Controller annotation on the ConstructorInjectedController class. @Autowired not used on the constructor as Spring uses the constructor by default

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,10 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingsService greetingsService;
