64ff9ae491b8c39ea2cc5bf44b8c3e90139f3d36
ChrisPerkinsCap <chrisperkins.cap@gmail.com>
2023-04-19 22:17:50 +0200

    My solutions: Added Constructor injection to the PetController class and annotated the class with the @Controller annotation.

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,24 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 12/28/19.
+ * Added the @Controller annotation to make spring aware of the PetController bean. cp on 19-04-23
  */
+@Controller
 public class PetController {
 
     private final PetService petService;
+
+    /**
+     * Added the constructor to inject the petService dependency into the PetController
+     * @param petService
+     */
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
